69658790c2f9190993be1d73e0f2095ce9cd00fd
Kruemmelspalter <kruemmelspalter@duck.com>
2023-12-01 21:22:05 -0800

    day 2 (kind of janky but only because i don't know how to use regex properly)

--- day-02/src/part1.rs
+++ day-02/src/part1.rs
@@ -1,5 +1,47 @@
 use color_eyre::eyre::Result;
 
 pub fn process(s: &str) -> Result<i64> {
-    todo!()
+    let lines = s.lines();
+
+    let mut res: i64 = 0;
+
+    for l in lines {
+        if l.is_empty() {
+            continue;
+        }
+
+        let mut split = l.split(|c| [':', ';'].contains(&c));
+        let (num, draws): (&str, Vec<&str>) = (split.next().unwrap(), split.collect());
+
+        let game_num = num.split(' ').last().unwrap().parse::<i64>().unwrap();
+
+        let mut green = 0;
+        let mut red = 0;
+        let mut blue = 0;
+
+        for d in draws {
+            let cols = d
+                .split(',')
+                .map(|x| x.trim().split(' '))
+                .map(|mut x| (x.next().unwrap(), x.next().unwrap()))
+                .collect::<Vec<_>>();
+
+            for col in cols {
+                let num = col.0.parse::<u8>().unwrap();
+                match col.1 {
+                    "green" if num > green => green = num,
+                    "red" if num > red => red = num,
+                    "blue" if num > blue => blue = num,
+                    _ => {}
+                }
+            }
+        }
+
+        // println!("{}: {} {} {}", game_num, red, green, blue);
+
+        if red <= 12 && green <= 13 && blue <= 14 {
+            res += game_num;
+        }
+    }
+    Ok(res)
 }
--- day-02/src/part2.rs
+++ day-02/src/part2.rs
@@ -1,5 +1,45 @@
 use color_eyre::eyre::Result;
 
 pub fn process(s: &str) -> Result<i64> {
-    todo!()
+    let lines = s.lines();
+
+    let mut res: i64 = 0;
+
+    for l in lines {
+        if l.is_empty() {
+            continue;
+        }
+
+        let mut split = l.split(|c| [':', ';'].contains(&c));
+        let (num, draws): (&str, Vec<&str>) = (split.next().unwrap(), split.collect());
+
+        let game_num = num.split(' ').last().unwrap().parse::<i64>().unwrap();
+
+        let mut green = 0;
+        let mut red = 0;
+        let mut blue = 0;
+
+        for d in draws {
+            let cols = d
+                .split(',')
+                .map(|x| x.trim().split(' '))
+                .map(|mut x| (x.next().unwrap(), x.next().unwrap()))
+                .collect::<Vec<_>>();
+
+            for col in cols {
+                let num = col.0.parse::<i64>().unwrap();
+                match col.1 {
+                    "green" if num > green => green = num,
+                    "red" if num > red => red = num,
+                    "blue" if num > blue => blue = num,
+                    _ => {}
+                }
+            }
+        }
+
+        // println!("{}: {} {} {}", game_num, red, green, blue);
+
+        res += red * green * blue;
+    }
+    Ok(res)
 }
