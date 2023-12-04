8ec576a93a76c24aac756d53728abeeaf5ddad57
Kruemmelspalter <kruemmelspalter@duck.com>
2023-12-03 21:27:37 -0800

    day 4 (not janky at all, surprisingly)

--- day-04/src/part1.rs
+++ day-04/src/part1.rs
@@ -1,5 +1,38 @@
 use color_eyre::eyre::Result;
 
 pub fn process(s: &str) -> Result<i64> {
-    todo!()
+    let cards = s
+        .lines()
+        .filter(|l| !l.is_empty())
+        .map(|l| l.split([':', '|']))
+        .map(|s| {
+            let mut s = s.skip(1);
+            (
+                parse_numbers(s.next().unwrap()),
+                parse_numbers(s.next().unwrap()),
+            )
+        })
+        .collect::<Vec<_>>();
+
+    let mut res = 0i64;
+
+    for card in cards {
+        let mut winning = 0;
+        for win in card.0.iter() {
+            for have in card.1.iter() {
+                if *win == *have {
+                    winning += 1;
+                }
+            }
+        }
+
+        res += 2i64.pow(winning) / 2;
+    }
+    Ok(res)
+}
+
+fn parse_numbers(s: &str) -> Vec<i64> {
+    s.split_whitespace()
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect()
 }
--- day-04/src/part2.rs
+++ day-04/src/part2.rs
@@ -1,5 +1,41 @@
+use std::cmp::min;
+
 use color_eyre::eyre::Result;
 
 pub fn process(s: &str) -> Result<i64> {
-    todo!()
+    let cards = s
+        .lines()
+        .filter(|l| !l.is_empty())
+        .map(|l| l.split([':', '|']))
+        .map(|s| {
+            let mut s = s.skip(1);
+            (
+                parse_numbers(s.next().unwrap()),
+                parse_numbers(s.next().unwrap()),
+            )
+        })
+        .collect::<Vec<_>>();
+
+    let mut amounts = vec![1; cards.len()];
+
+    for (i, card) in cards.iter().enumerate() {
+        let mut winning = 0;
+        for win in card.0.iter() {
+            for have in card.1.iter() {
+                if *win == *have {
+                    winning += 1;
+                }
+            }
+        }
+        for j in i + 1..=min(i + winning, cards.len() - 1) {
+            amounts[j] += amounts[i];
+        }
+    }
+    Ok(amounts.iter().sum())
+}
+
+fn parse_numbers(s: &str) -> Vec<i64> {
+    s.split_whitespace()
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect()
 }
